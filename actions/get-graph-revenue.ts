363a2c31e67e4631fe3b614da3bceee855696b57
d2217bb <daniela.boros@isa.utm.md>
2025-09-19 20:23:55 +0300

    update get graph

--- actions/get-graph-revenue.ts
+++ actions/get-graph-revenue.ts
@@ -4,3 +4,19 @@ interface GraphData {
   name: string;
   total: number;
 }
+
+export const getGraphRevenue = async (storeId: string) => {
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      storeId,
+      isPaid: true,
+    },
+
+    include: {
+      orderItems: {
+        include: { product: true },
+      },
+    },
+  });
+
+  
